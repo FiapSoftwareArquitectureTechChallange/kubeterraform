14e21804d2467c268fd7fc8449fe578e78650f3f
Bruno Cardoso <bruno.cardoso@finplace.com.br>
2024-03-19 22:06:19 -0300

    add forcedelete ECR

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name = "${var.project_name}-ecr"
-
+  force_delete = true
   tags = {
     "Name"        = "${var.project_name}-ecr"
     "Environment" = "development"
