8887598cfe3e6b1fbf0c2c626f9282c869df55cc
Douglas Fuelber <dev@douglasfuelber.com>
2024-05-18 13:18:32 -0300

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -6,3 +6,21 @@ resource "aws_ecr_repository" "ecr_repository" {
     "Environment" = "development"
   }
 }
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name         = "${var.project_name}-payment-ecr"
+  force_delete = true
+  tags = {
+    "Name"        = "${var.project_name}-payment-ecr"
+    "Environment" = "development"
+  }
+}
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name         = "${var.project_name}-kitchen-ecr"
+  force_delete = true
+  tags = {
+    "Name"        = "${var.project_name}-kitchen-ecr"
+    "Environment" = "development"
+  }
+}
